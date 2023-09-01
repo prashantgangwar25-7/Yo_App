030c0201040bad3a23443e5cf96a308df6c2dc84
Prashant Gangwar <prashantgangwar45@gmail.com>
2023-09-01 20:38:49 +0530

    Initial commit

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";//use to render react components
+import App from "./App";
+import "./index.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ server/controllers/messageController.js
@@ -0,0 +1,39 @@
+const Messages = require("../models/messageModel");
+
+module.exports.getMessages = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },//will select all messages that have from(ID) and to(ID) not in order
+    }).sort({ updatedAt: 1 });
+
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,//true or false
+        message: msg.message.text,
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) return res.json({ msg: "Message added successfully." });
+    else return res.json({ msg: "Failed to add message to the database" });
+  } catch (ex) {
+    next(ex);
+  }
+};
