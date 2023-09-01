e155170dbcba6d768d08f01e0381fbb94dc77b75
Prashant Gangwar <prashantgangwar45@gmail.com>
2023-09-01 20:56:49 +0530

    Update APIRoutes.js
    host URL added

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,6 @@
 // export const host = "https://yo-prashant-gangwar.herokuapp.com";//heroku link
-export const host = "http://localhost:5000";//heroku link
+// export const host = "http://localhost:5000";//heroku link
+export const host = "https://yo-0i0b.onrender.com";//render link
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
